f95ea87c2d43ca198cdeaa7dd4d99aa20499758d
Cabral-it <cabral.it28@gmail.com>
2024-06-04 16:31:57 +0000

    Feat : gestion des plaintes , db and seed

---
+++ database/migrations/2024_06_03_143830_create_plaintes_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaintesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plaintes', function (Blueprint $table) {
+            $table->id();
+            // Section 1 Localisation reclamation
+            $table->string('localite')->nullable();
+            $table->string('commune')->nullable();
+            $table->string('departement')->nullable();
+            $table->string('region')->nullable();
+            // Section 2 Identification du plaignant
+            $table->string('prenom')->nullable();
+            $table->string('nom')->nullable();
+            $table->boolean('sexe')->nullable();
+            $table->string('fonction')->nullable();
+            $table->unsignedBigInteger('identification_id')->nullable(); // cni
+            $table->string('identification_numero')->nullable(); // numero cni
+            $table->unsignedBigInteger('entreprise_id')->nullable();
+            $table->string('entreprise_name')->nullable();
+            $table->string('entreprise_contact')->nullable();
+            $table->bigInteger('entreprise_telephone')->nullable();
+            $table->string('entreprise_email')->nullable();
+            // Section 3 Description de la réclamation
+            $table->unsignedBigInteger('reclamation_type')->nullable();
+            $table->text('reclamation_synthese')->nullable();
+            $table->date('incident_date');
+            $table->text('incident_description')->nullable();
+            $table->text('incident_happen')->nullable();
+            $table->string('incident_lieu')->nullable();
+            $table->string('incident_victime')->nullable();
+            $table->text('consequences')->nullable();
+            $table->text('solutions')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('pj')->nullable();
+            $table->string('signature')->nullable();
+            // Section 4 Coordonnées GPS de la réclamation
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('alttitude')->nullable();
+            $table->string('precision')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('identification_id')->references('id')->on('identifications');
+            $table->foreign('entreprise_id')->references('id')->on('entreprises');
+            $table->foreign('reclamation_type')->references('id')->on('reclamations');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plaintes');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\Traits\TruncateTable;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Entreprise;
+use App\Models\Reclamation;
+use App\Models\Identification;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Database\Seeders\Traits\TruncateTable;
 
 /**
  * Class DatabaseSeeder.
@@ -27,6 +30,32 @@ class DatabaseSeeder extends Seeder
 
         $this->call(AuthSeeder::class);
         $this->call(AnnouncementSeeder::class);
+
+        // Seed des identifications type
+        Identification::create(['libele' => 'Carte nationale D\'identification']);
+        Identification::create(['libele' => 'Passport']);
+        Identification::create(['libele' => 'Carte consulaire']);
+        Identification::create(['libele' => 'Registre de commerce']);
+        Identification::create(['libele' => 'NINEA']);
+        Identification::create(['libele' => 'Titre de propriété']);
+        Identification::create(['libele' => 'Status de l\'entreprise']);
+        Identification::create(['libele' => 'Autre pièce d\'identification']);
+        // Seed des entreprises type
+        Entreprise::create(['libele' => 'Personne physique']);
+        Entreprise::create(['libele' => 'Famille']);
+        Entreprise::create(['libele' => 'Entreprise']);
+        Entreprise::create(['libele' => 'GIE']);
+        Entreprise::create(['libele' => 'Association ou groupement (OCB)']);
+        Entreprise::create(['libele' => 'Structure communautaire ou ONG']);
+        Entreprise::create(['libele' => 'Service étatique']);
+        Entreprise::create(['libele' => 'Autre Catégorie']);
+        // Seed des entreprises type
+        Reclamation::create(['libele' => 'Opération de sécurisation foncière']);
+        Reclamation::create(['libele' => 'Identification ou enregistrement des droits']);
+        Reclamation::create(['libele' => 'Travaux de construction du bureau foncier']);
+        Reclamation::create(['libele' => 'Personnel projet']);
+        Reclamation::create(['libele' => 'Autre réclamation']);
+
 
         Model::reguard();
     }
